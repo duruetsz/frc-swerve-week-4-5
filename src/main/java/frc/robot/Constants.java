7660ad6e70758390981b0d21911c4b2c31c2a102
duruetsz <duruelif005@gmail.com>
2026-04-14 16:43:29 +0300

    just started

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class Constants {
+    public class ShooterConstants {
+
+    }
+
+    public class IntakeConstants {
+
+    }
+
+    public class LimelightConstants {
+
+    }
+    
+}
