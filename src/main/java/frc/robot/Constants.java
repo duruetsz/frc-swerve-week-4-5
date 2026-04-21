a1b3d8397bcc7b6274870218af339c7b2a3f0748
Duru Elif Tüsüz <duruelif005@gmail.com>
2026-04-21 17:54:19 +0000

    i tried using codespace on mobile

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,7 +2,12 @@ package frc.robot;
 
 public class Constants {
     public class ShooterConstants {
-
+        public static final int shooterMotorID = 9;
+        public static final double shooterP = 0.1;
+        public static final double shooterI = 0.02;
+        public static final double shooterD = 0.00;
+        public static final double shooterS = 0.10;
+        public static final double shooterV = 0.10;
     }
 
     public class IntakeConstants {
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,11 +5,38 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.ShooterConstants.*;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
 
 public class ShooterSubsystem extends SubsystemBase {
-  public ShooterSubsystem() {}
+  private TalonFX shooterMotor;
+  private TalonFXConfiguration shooterConfig = new TalonFXConfiguration();
+  var slot0 = shooterConfig.slot0;
+  public final VoltageOut shooterVoltageOut = new VoltageOut(0);
+  
+  public ShooterSubsystem() {
+    shooterMotor = new TalonFX(shooterMotorID);
+    
+    
+  }
 
   @Override
   public void periodic() {
+  }
+
+  public void configureHardware() {
+    shooterConfig.slot0.kP = shooterP;
+    shooterConfig.slot0.kI = shooterI;
+    shooterConfig.slot0.kD = shooterD;
+    shooterConfig.slot0.kS = shooterS;
+    shooterConfig.slot0.kV = shooterV;
+    shooterMotor.getConfigurator().apply(shooterConfig);
+  }
+
+  public void setShooterVoltage(double voltage) {
+    shooterMotor.setControl(shooterVoltageOut.withOutput(voltage));
   }
 }
